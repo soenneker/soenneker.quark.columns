c4df07cffbd5321b83efe4d71795e43e49afed78
Jake Soenneker <jake@soenneker.com>
2025-09-26 20:46:11 -0500

    enum renames

--- src/Enums/ColumnOrderType.cs
+++ src/Enums/ColumnOrderType.cs
@@ -7,89 +7,89 @@ namespace Soenneker.Quark.Enums;
 /// This enum contains the standard Bootstrap column order values used for grid layout.
 /// </summary>
 [Intellenum<string>]
-public partial class ColumnOrder
+public partial class ColumnOrderType
 {
     /// <summary>
     /// Column order 1.
     /// Bootstrap class: order-1
     /// </summary>
-    public static readonly ColumnOrder O1 = new("1");
+    public static readonly ColumnOrderType O1 = new("1");
 
     /// <summary>
     /// Column order 2.
     /// Bootstrap class: order-2
     /// </summary>
-    public static readonly ColumnOrder O2 = new("2");
+    public static readonly ColumnOrderType O2 = new("2");
 
     /// <summary>
     /// Column order 3.
     /// Bootstrap class: order-3
     /// </summary>
-    public static readonly ColumnOrder O3 = new("3");
+    public static readonly ColumnOrderType O3 = new("3");
 
     /// <summary>
     /// Column order 4.
     /// Bootstrap class: order-4
     /// </summary>
-    public static readonly ColumnOrder O4 = new("4");
+    public static readonly ColumnOrderType O4 = new("4");
 
     /// <summary>
     /// Column order 5.
     /// Bootstrap class: order-5
     /// </summary>
-    public static readonly ColumnOrder O5 = new("5");
+    public static readonly ColumnOrderType O5 = new("5");
 
     /// <summary>
     /// Column order 6.
     /// Bootstrap class: order-6
     /// </summary>
-    public static readonly ColumnOrder O6 = new("6");
+    public static readonly ColumnOrderType O6 = new("6");
 
     /// <summary>
     /// Column order 7.
     /// Bootstrap class: order-7
     /// </summary>
-    public static readonly ColumnOrder O7 = new("7");
+    public static readonly ColumnOrderType O7 = new("7");
 
     /// <summary>
     /// Column order 8.
     /// Bootstrap class: order-8
     /// </summary>
-    public static readonly ColumnOrder O8 = new("8");
+    public static readonly ColumnOrderType O8 = new("8");
 
     /// <summary>
     /// Column order 9.
     /// Bootstrap class: order-9
     /// </summary>
-    public static readonly ColumnOrder O9 = new("9");
+    public static readonly ColumnOrderType O9 = new("9");
 
     /// <summary>
     /// Column order 10.
     /// Bootstrap class: order-10
     /// </summary>
-    public static readonly ColumnOrder O10 = new("10");
+    public static readonly ColumnOrderType O10 = new("10");
 
     /// <summary>
     /// Column order 11.
     /// Bootstrap class: order-11
     /// </summary>
-    public static readonly ColumnOrder O11 = new("11");
+    public static readonly ColumnOrderType O11 = new("11");
 
     /// <summary>
     /// Column order 12.
     /// Bootstrap class: order-12
     /// </summary>
-    public static readonly ColumnOrder O12 = new("12");
+    public static readonly ColumnOrderType O12 = new("12");
 
     /// <summary>
     /// Column order first.
     /// Bootstrap class: order-first
     /// </summary>
-    public static readonly ColumnOrder First = new("first");
+    public static readonly ColumnOrderType First = new("first");
 
     /// <summary>
     /// Column order last.
     /// Bootstrap class: order-last
     /// </summary>
-    public static readonly ColumnOrder Last = new("last");
+    public static readonly ColumnOrderType Last = new("last");
 }
--- src/Enums/ColumnSizeType.cs
+++ src/Enums/ColumnSizeType.cs
@@ -7,83 +7,83 @@ namespace Soenneker.Quark.Enums;
 /// This enum contains the standard Bootstrap column size values used for grid layout.
 /// </summary>
 [Intellenum<string>]
-public partial class ColumnSize
+public sealed partial class ColumnSizeType
 {
     /// <summary>
     /// Column size 1 (1/12 of the grid).
     /// Bootstrap class: col-1
     /// </summary>
-    public static readonly ColumnSize S1 = new("1");
+    public static readonly ColumnSizeType S1 = new("1");
 
     /// <summary>
     /// Column size 2 (2/12 of the grid).
     /// Bootstrap class: col-2
     /// </summary>
-    public static readonly ColumnSize S2 = new("2");
+    public static readonly ColumnSizeType S2 = new("2");
 
     /// <summary>
     /// Column size 3 (3/12 of the grid).
     /// Bootstrap class: col-3
     /// </summary>
-    public static readonly ColumnSize S3 = new("3");
+    public static readonly ColumnSizeType S3 = new("3");
 
     /// <summary>
     /// Column size 4 (4/12 of the grid).
     /// Bootstrap class: col-4
     /// </summary>
-    public static readonly ColumnSize S4 = new("4");
+    public static readonly ColumnSizeType S4 = new("4");
 
     /// <summary>
     /// Column size 5 (5/12 of the grid).
     /// Bootstrap class: col-5
     /// </summary>
-    public static readonly ColumnSize S5 = new("5");
+    public static readonly ColumnSizeType S5 = new("5");
 
     /// <summary>
     /// Column size 6 (6/12 of the grid).
     /// Bootstrap class: col-6
     /// </summary>
-    public static readonly ColumnSize S6 = new("6");
+    public static readonly ColumnSizeType S6 = new("6");
 
     /// <summary>
     /// Column size 7 (7/12 of the grid).
     /// Bootstrap class: col-7
     /// </summary>
-    public static readonly ColumnSize S7 = new("7");
+    public static readonly ColumnSizeType S7 = new("7");
 
     /// <summary>
     /// Column size 8 (8/12 of the grid).
     /// Bootstrap class: col-8
     /// </summary>
-    public static readonly ColumnSize S8 = new("8");
+    public static readonly ColumnSizeType S8 = new("8");
 
     /// <summary>
     /// Column size 9 (9/12 of the grid).
     /// Bootstrap class: col-9
     /// </summary>
-    public static readonly ColumnSize S9 = new("9");
+    public static readonly ColumnSizeType S9 = new("9");
 
     /// <summary>
     /// Column size 10 (10/12 of the grid).
     /// Bootstrap class: col-10
     /// </summary>
-    public static readonly ColumnSize S10 = new("10");
+    public static readonly ColumnSizeType S10 = new("10");
 
     /// <summary>
     /// Column size 11 (11/12 of the grid).
     /// Bootstrap class: col-11
     /// </summary>
-    public static readonly ColumnSize S11 = new("11");
+    public static readonly ColumnSizeType S11 = new("11");
 
     /// <summary>
     /// Column size 12 (12/12 of the grid - full width).
     /// Bootstrap class: col-12
     /// </summary>
-    public static readonly ColumnSize S12 = new("12");
+    public static readonly ColumnSizeType S12 = new("12");
 
     /// <summary>
     /// Auto column size (takes remaining space).
     /// Bootstrap class: col-auto
     /// </summary>
-    public static readonly ColumnSize Auto = new("auto");
+    public static readonly ColumnSizeType Auto = new("auto");
 }
